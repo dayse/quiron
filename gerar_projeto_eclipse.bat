9eacfa0ff99649fd79baa7b43ea62ed0595c5c28
Felipe Arruda Pontes <felipe.pontes@int.gov.br>
2014-04-29 12:22:51 -0300

    Script de Importação Colocando mais uma coisa no script, para que ele copie automaticamente o arquivo de configuração da carga de usuarios. refs #36

--- gerar_projeto_eclipse.bat
+++ gerar_projeto_eclipse.bat
@@ -1,2 +1,4 @@
-xcopy .settings.example .settings /E /I
+copy WebContent\WEB-INF\carga\usuarios.json.example WebContent\WEB-INF\carga\usuarios.json
 copy .classpath.example .classpath
+xcopy .settings.example .settings /E /I
+
