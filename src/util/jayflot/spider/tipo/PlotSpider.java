50f7c6be45570f4810a2be7f06059cce4d09d8dc
Felipe Arruda Pontes <felipe.pontes@int.gov.br>
2014-10-29 12:40:55 -0200

    JayFlot - Herdar e sobreescrever classes Criado classe de `tipo.PlotSpider` com os campos necessários. refs #155

--- src/util/jayflot/spider/tipo/PlotSpider.java
+++ src/util/jayflot/spider/tipo/PlotSpider.java
@@ -1,28 +1,8 @@
 package util.jayflot.spider.tipo;
+import java.util.ArrayList;
+
 import br.blog.arruda.plot.opt.tipo.PlotType;
 
-
-//pointSize: 5,
-//connection: {width:5},
-//active: true, 
-//highlight: {mode: "area"},
-//legs: { 
-//  data: [
-//    {label: "Efeitos Colaterais"},
-//    {label: "Febre"},
-//    {label: "Disuria"},
-//    {label: "Diabetes"},
-//    {label: "P005"},
-//    {label: "P006"},
-//    {label: "P007"},
-//    {label: "P008"}
-//    ],
-//  // legScaleMax: 1,legScaleMin:0.8
-//},
-//scaleMode: "static" ,
-//legMin: 0,
-//legMax: 100,
-//spiderSize: 0.5   
 
 /**
  * PlotSpider com opções relativas ao pluging Spider (grafico de Radar/Spider)
@@ -39,28 +19,141 @@ public class PlotSpider extends PlotType{
 	/** active: true **/
 	private Boolean active;
 	
-	/** highlight: {mode: "area"}**/
+	/** highlight: {mode: "area"} **/
 	private SpiderHighlight highlight;
 	
-	/** **/
-//	private SpiderLegs legs;
+	/** 'legs' do grafico spider/radar **/
+	private SpiderLeg legs;
 	
-	/** ex: static **/
+	/** ex: scaleMode: "static" **/
 	private String scaleMode;
 	
+	/** ex: legMin: 0 **/
+	private Double legMin;
 	
+	/** ex: legMax: 100 **/
+	private Double legMax;
+	
+	/** ex: spiderSize: 0.5 **/
+	private Double spiderSize;
+	
+
+	public PlotSpider() {
+		super();
+		this.pointSize = null;
+		this.connection = null;
+		this.active = null;
+		this.highlight = null;
+		this.legs = null;
+		this.scaleMode = null;
+		this.legMin = null;
+		this.legMax = null;
+		this.spiderSize = null;
+	}
+
+	public Double getPointSize() {
+		return pointSize;
+	}
+
+	public void setPointSize(Double pointSize) {
+		this.pointSize = pointSize;
+	}
+
+	public SpiderConnection getConnection() {
+		return connection;
+	}
+
+	public void setConnection(SpiderConnection connection) {
+		this.connection = connection;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public SpiderHighlight getHighlight() {
+		return highlight;
+	}
+
+	public void setHighlight(SpiderHighlight highlight) {
+		this.highlight = highlight;
+	}
+
+	public SpiderLeg getLegs() {
+		return legs;
+	}
+
+	public void setLegs(SpiderLeg legs) {
+		this.legs = legs;
+	}
+
+	public String getScaleMode() {
+		return scaleMode;
+	}
+
+	public void setScaleMode(String scaleMode) {
+		this.scaleMode = scaleMode;
+	}
+
+	public Double getLegMin() {
+		return legMin;
+	}
+
+	public void setLegMin(Double legMin) {
+		this.legMin = legMin;
+	}
+
+	public Double getLegMax() {
+		return legMax;
+	}
+
+	public void setLegMax(Double legMax) {
+		this.legMax = legMax;
+	}
+
+	public Double getSpiderSize() {
+		return spiderSize;
+	}
+
+	public void setSpiderSize(Double spiderSize) {
+		this.spiderSize = spiderSize;
+	}
+
+	//Classes utilizadas para montar as opções mais complexas de SpiderPlot //
 	public class SpiderConnection{
 		private Double width;
 	};
 	
 	public class SpiderHighlight{
-		/** ex: area **/
+		/** ex: mode: "area" **/
 		private String mode;
 	};
 
 	public class SpiderLeg{
-		/** ex: area **/
-		private String data;
+		private ArrayList<SpiderLegData> data;
+		
+		/**
+		 * Cria um conjunto de dados das 'pernas' do grafico,
+		 * dado os nomes das labels de cada uma 
+		 * @param labels
+		 */
+		public void setData(ArrayList<String> labels){
+			for (String label : labels) {
+				this.data.add(new SpiderLegData(label));
+			}
+		}
+	};
+
+	public class SpiderLegData {
+		/** ex: label: "Efeitos Colaterais" **/
+		private String label;
+		public SpiderLegData(String label){
+			this.label = label;
+		}
 	};
 	
 
