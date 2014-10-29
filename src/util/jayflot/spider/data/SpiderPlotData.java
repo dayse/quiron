eb1ad9b6efef3ef60cf579a3553d46a1c4e15ee6
Felipe Arruda Pontes <felipe.pontes@int.gov.br>
2014-10-29 14:53:21 -0200

    colocando javadocs no SpiderPlotData

--- src/util/jayflot/spider/data/SpiderPlotData.java
+++ src/util/jayflot/spider/data/SpiderPlotData.java
@@ -2,6 +2,10 @@ package util.jayflot.spider.data;
 import util.jayflot.spider.tipo.PlotSpider;
 import br.blog.arruda.plot.data.PlotData;
 
+/**
+ * SpiderPlotData com opções relativas ao pluging Spider (grafico de Radar/Spider)
+ * @author arruda
+ */
 public class SpiderPlotData extends PlotData{
 
 	private PlotSpider spider;
