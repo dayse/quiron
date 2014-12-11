8b3c18b02b39812bcac24047ca7fbe9426ed3a82
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-12-10 22:30:30 -0200

    arrumando detalhe da verificação do proxy nos dockers e arrumando o que faltou arrumar no docker do pgdoc. refs #172

--- dockers/docker-pgdoc/scripts/check_proxy_int.sh
+++ dockers/docker-pgdoc/scripts/check_proxy_int.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if ping -c 1 http://proxy.corp.int.gov.br ; then
+if ping -c 1 www.google.com.br ; then
+    echo "direct internet doing nothing"
+else
     echo "proxy environment detected setting proxy"
     export http_proxy=http://felipe.pontes:qualquercoisa@proxy.corp.int.gov.br:3128
     export https_proxy=http://felipe.pontes:qualquercoisa@proxy.corp.int.gov.br:3128
     echo 'Acquire::http::Proxy "http://felipe.pontes:qualquercoisa@proxy.corp.int.gov.br:3128";' >> "/etc/apt/apt.conf.d/01proxy"
     echo 'Acquire::https::Proxy "http://felipe.pontes:qualquercoisa@proxy.corp.int.gov.br:3128";' >> "/etc/apt/apt.conf.d/01proxy"
-else
-    echo "direct internet doing nothing"
 fi
--- dockers/docker-pgdoc/scripts/start.sh
+++ dockers/docker-pgdoc/scripts/start.sh
@@ -7,8 +7,8 @@ DATA_DIR=/data
 
 
 # Start postgresql_autodoc
-echo "Generating png for $DB_PORT_5432_TCP_ADDR $DB_ENV_DB"
-echo "User and pass: $DB_ENV_USER $DB_ENV_PASS"
+echo "Generating png for $DB_PORT_5432_TCP_ADDR $DB_ENV_DB_NAME"
+echo "User and pass: $DB_ENV_POSTGRES_USER $DB_ENV_POSTGRES_PASSWORD"
 
-postgresql_autodoc -t dot -h $DB_PORT_5432_TCP_ADDR -d $DB_ENV_DB -u $DB_ENV_USER --password=$DB_ENV_PASS
-dot -Tpng $DATA_DIR/$DB_ENV_DB.dot -o $DATA_DIR/$DB_ENV_DB.png
+postgresql_autodoc -t dot -h $DB_PORT_5432_TCP_ADDR -d $DB_ENV_DB_NAME -u $DB_ENV_POSTGRES_USER --password=$DB_ENV_POSTGRES_PASSWORD
+dot -Tpng $DATA_DIR/$DB_ENV_DB_NAME.dot -o $DATA_DIR/$DB_ENV_DB_NAME.png
