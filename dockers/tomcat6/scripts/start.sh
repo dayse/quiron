4c9cfcb942d62dc53101c86889449b6af037f26d
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-12-17 16:43:40 -0200

    forçando o tomcat a usar utf na hr de renderizar a pagina, é só uma garantia a mais entretanto isso não resolve o problema. 	refs #175

--- dockers/tomcat6/scripts/start.sh
+++ dockers/tomcat6/scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i -e"s/Connector port=\"8080\"/Connector port=\"8080\" URIEncoding=\"UTF-8\"/g" /usr/share/tomcat/conf/server.xml
+
 sed -i -e"s/username=\"tomcat\"/username=\""${MANAGER_USERNAME}"\"/g" /usr/share/tomcat/conf/tomcat-users.xml
 sed -i -e"s/password=\"tomcat\"/password=\""${MANAGER_PASSWORD}"\"/g" /usr/share/tomcat/conf/tomcat-users.xml
 
